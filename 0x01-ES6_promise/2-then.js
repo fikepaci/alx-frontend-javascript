0d03fc7c761eda50568708a0d626722812637ec7
fikepaci <pacifiquemachine@gmail.com>
2021-07-16 18:42:51 +0200

    add files

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+function handleResponseFromAPI(promise) {
+  return Promise.then(() => ({
+    status: 200,
+    body: 'success',
+  }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
