f952faccb3bc7035a07b720160d09475f35b1338
fikepaci <pacifiquemachine@gmail.com>
2021-07-17 02:07:26 +0200

    add files

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,18 +1,23 @@
-// async function that will call these two functions and return an object
-import { uploadPhoto, createUser } from '../utils';
+import { uploadPhoto, createUser } from './utils';
 
-export default async function asyncUploadUser() {
+async function asyncUploadUser() {
+  let photo;
+  let user;
+
   try {
-    const photo = await uploadPhoto();
-    const user = await createUser();
-    return {
-      photo,
-      user,
-    };
-  } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (err) {
+    photo = null;
+    user = null;
   }
+
+  const uploadUser = {
+    photo,
+    user,
+  };
+
+  return uploadUser;
 }
+
+export default asyncUploadUser;
