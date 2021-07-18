d6f99a57d704c44527374bc55dfac086a7ccacbc
fikepaci <pacifiquemachine@gmail.com>
2021-07-18 19:06:54 +0200

    update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,7 +3,7 @@ import Building from './5-building';
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
