db0eac7aa87a59c439eba2dfd245ad895a5a1098
fikepaci <pacifiquemachine@gmail.com>
2021-07-18 20:05:47 +0200

    add 10-cars.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+// car class
+export default class Car {
+  Constructor(brand = '', motor = '', color = '') {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.Constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
+  }
+}
