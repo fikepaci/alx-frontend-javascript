673bbf1d33e9f6fdd39ea4d817a861325247d4f0
fikepaci <pacifiquemachine@gmail.com>
2021-07-20 11:01:33 +0200

    add 0-get-list-students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+const getListStudents = () => {
+  const students = [
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 2, firstname: 'Serena', location: 'San Francisco' },
+  ];
+
+  return students;
+};
+
+export default getListStudents;
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,9 @@
+# ES6 data manipulation
+
+the JavaScript Array class is a global object that is used in the construction of arrays; which are high-level, list-like objects.
+
+## Learning Objectives
+
+- How to use map, filter and reduce on arrays
+- Typed arrays
+- The Set, Map, and Weak link data structures
