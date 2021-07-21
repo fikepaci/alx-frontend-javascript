b9c837fca3649f189b30ef0cfdf32f6e87f8752d
fikepaci <pacifiquemachine@gmail.com>
2021-07-22 00:48:45 +0200

    update 0-get_list_student.js

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,14 @@
+interface Student {
+  firstName?: string;
+  lastName?: string;
+  age?: number;
+  location?: string;
+}
+
+var student_1: string = "John petel";
+var student_2: string = "pacifique irakoze";
+
+var studentsList: string[];
+studentsList = [student_1, student_2];
+
+console.log(studentsList[0]);
